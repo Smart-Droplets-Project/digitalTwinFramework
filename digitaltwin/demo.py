ab8a5e38a070bbb567c72e5dce09ff19481b7636
Michiel Kallenberg <michielkallenberg@gmail.com>
2024-11-26 15:09:22 +0100

    minor bugfix

--- digitaltwin/demo.py
+++ digitaltwin/demo.py
@@ -1,30 +1,11 @@
 import argparse
 
 from sd_data_adapter.client import DAClient
-from sd_data_adapter.api import search, upsert
-from sd_data_adapter.models import AgriFood
-import sd_data_adapter.models.device as device_model
 
-from digitaltwin.cropmodel.crop_model import create_digital_twins, get_default_variables
-from digitaltwin.cropmodel.recommendation import standard_practice
-from digitaltwin.utils.data_adapter import (
-    create_command_message,
-    fill_database,
-    get_row_coordinates,
-    generate_rec_message_id,
-    get_recommendation_message,
-)
-from digitaltwin.utils.database import (
-    get_by_id,
-    get_demo_parcels,
-    has_demodata,
-    find_parcel_operations,
-    find_device,
-    find_device_measurement,
-    find_command_messages,
-    clear_database,
-    get_parcel_operation_by_date,
-)
+from digitaltwin.cropmodel.crop_model import get_default_variables
+from digitaltwin.utils.data_adapter import fill_database
+from digitaltwin.utils.database import has_demodata, clear_database
+from digitaltwin.utils.simulator import run_cropmodel
 
 
 def main():
