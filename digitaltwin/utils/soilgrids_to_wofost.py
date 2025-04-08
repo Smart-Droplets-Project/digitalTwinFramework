5ee7eadfa5cbd0b5a2644c5cd80d482f01b0eadf
Michiel Kallenberg <michielkallenberg@gmail.com>
2025-04-08 11:45:41 +0200

    soilgrid_to_wofost from master

--- digitaltwin/utils/soilgrids_to_wofost.py
+++ digitaltwin/utils/soilgrids_to_wofost.py
@@ -1,7 +1,7 @@
 """
-Code borrowed from Allard de Wit and Herman Berghuijs
-Wageningen University & Research
-"""
+ Code borrowed from Allard de Wit and Herman Berghuijs
+ Wageningen University & Research
+ """
 
 import argparse
 import os
@@ -16,6 +16,7 @@ import pandas as pd
 CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 SOURCE_FOLDER = os.path.dirname(CURRENT_FOLDER)
 soil_save_dir = os.path.join(SOURCE_FOLDER, "cropmodel", "configs", "soil")
+
 
 def default_soilgrid_variables():
     # Define variables that need to be collected for this location
@@ -65,11 +66,9 @@ def calculate_is_topsoil(zmin, zmax, zmax_topsoil):
 def calculate_van_genuchten(
         df_soilgrids: pd.DataFrame,
         lower_boundary_top_soil: float = 100.,
-    ) -> pd.DataFrame:
-
+) -> pd.DataFrame:
     f_C_to_OM = default_som_content()
     pml_to_pct = 0.1
-
 
     ptfw = PedotransferFunctionsWosten()
     df_vgp = df_soilgrids.copy()[["latitude", "longitude", "zmin", "zmax", "soc", "phh2o", "nitrogen"]]
@@ -151,42 +150,42 @@ def generate_soil_yaml(
 
     # below we generate the header of the soil input file as YAML input structure
     soil_input_yaml = f"""
-    RDMSOL: {sum(Thickness)}
-    SoilProfileDescription:
-        PFWiltingPoint: {PFWiltingPoint}
-        PFFieldCapacity: {PFFieldCapacity}
-        SurfaceConductivity: {SurfaceConductivity}
-        GroundWater: false
-        SoilLayers:
-    """
+     RDMSOL: {sum(Thickness)}
+     SoilProfileDescription:
+         PFWiltingPoint: {PFWiltingPoint}
+         PFFieldCapacity: {PFFieldCapacity}
+         SurfaceConductivity: {SurfaceConductivity}
+         GroundWater: false
+         SoilLayers:
+     """
 
     # Here we generate the properties for each soil layer including layer thickness, hydraulic properties,
     # organic matter content, etc.
     for i in range(nlayers):
         s = f"""    - Thickness: {Thickness[i]}
-          CNRatioSOMI: {CNRatioSOMI[i]}
-          CRAIRC: {CRAIRC[i]}
-          FSOMI: {FSOMI[i]}
-          RHOD: {RHOD[i]}
-          Soil_pH: {Soil_pH[i]}
-          SMfromPF: {make_string_table(SMfromPF_perlayer[i])}
-          CONDfromPF: {make_string_table(CONDfromPF_perlayer[i])}
-    """
+           CNRatioSOMI: {CNRatioSOMI[i]}
+           CRAIRC: {CRAIRC[i]}
+           FSOMI: {FSOMI[i]}
+           RHOD: {RHOD[i]}
+           Soil_pH: {Soil_pH[i]}
+           SMfromPF: {make_string_table(SMfromPF_perlayer[i])}
+           CONDfromPF: {make_string_table(CONDfromPF_perlayer[i])}
+     """
         soil_input_yaml += s
 
     # A SubSoilType needs to be defined. In this case we make the subsoil equal to the properties
     # of the deepest soil layer.
     soil_input_yaml += \
         f"""    SubSoilType:
-          CNRatioSOMI: {CNRatioSOMI[-1]}
-          CRAIRC: {CRAIRC[-1]}
-          FSOMI: {FSOMI[-1]}
-          RHOD: {RHOD[-1]}
-          Soil_pH: {Soil_pH[-1]}
-          Thickness: {Thickness[-1]}
-          SMfromPF: {make_string_table(SMfromPF_perlayer[-1])}
-          CONDfromPF: {make_string_table(CONDfromPF_perlayer[-1])}
-    """
+           CNRatioSOMI: {CNRatioSOMI[-1]}
+           CRAIRC: {CRAIRC[-1]}
+           FSOMI: {FSOMI[-1]}
+           RHOD: {RHOD[-1]}
+           Soil_pH: {Soil_pH[-1]}
+           Thickness: {Thickness[-1]}
+           SMfromPF: {make_string_table(SMfromPF_perlayer[-1])}
+           CONDfromPF: {make_string_table(CONDfromPF_perlayer[-1])}
+     """
 
     soil_dict = yaml.safe_load(soil_input_yaml)
     soil_yaml = json.dumps(soil_dict, indent=6)
@@ -203,8 +202,9 @@ class PedotransferFunctionsWosten():
     """
     Taken from https://github.com/ajwdewit/pcse_notebooks/
     """
+
     def calculate_van_genuchten_parameters(self, C, D, S, OM, theta_r, topSoil):
-        if(OM == 0):
+        if (OM == 0):
             OM = 0.01
         dict_vg = {}
         dict_vg["alpha"] = self.calculate_alpha(C, D, S, OM, topSoil)
@@ -230,54 +230,60 @@ class PedotransferFunctionsWosten():
         labda = (10 * np.exp(t_labda) - 10) / (1 + np.exp(t_labda))
         return labda
 
-    def calculate_k_sat(self,  C, D, S, OM, topSoil):
-        t_k_sat = self.calculate_transformed_ksat( C, D, S, OM, topSoil)
+    def calculate_k_sat(self, C, D, S, OM, topSoil):
+        t_k_sat = self.calculate_transformed_ksat(C, D, S, OM, topSoil)
         k_sat = np.exp(t_k_sat)
         return k_sat
 
     def calculate_theta_s(self, C, D, S, OM, topSoil):
-        if(topSoil):
+        if (topSoil):
             TS = 1.
         else:
             TS = 0.
-        theta_s = 0.7919 + 0.001691 * C - 0.29619 * D - 0.000001491 * S * S + 0.0000821 * OM * OM + 0.02427 * (1 / C) + 0.01113 * (1 / S) + \
-                0.01472 * np.log(S) - 0.0000733 * OM * C - 0.000619 * D * C - 0.001183 * D * OM - 0.0001664 * topSoil * S
+        theta_s = 0.7919 + 0.001691 * C - 0.29619 * D - 0.000001491 * S * S + 0.0000821 * OM * OM + 0.02427 * (
+                    1 / C) + 0.01113 * (1 / S) + \
+                  0.01472 * np.log(
+            S) - 0.0000733 * OM * C - 0.000619 * D * C - 0.001183 * D * OM - 0.0001664 * topSoil * S
         return theta_s
 
     def calculate_transformed_alpha(self, C, D, S, OM, topSoil):
-        if(topSoil):
+        if (topSoil):
             TS = 1.
         else:
             TS = 0.
         t_alpha = -14.96 + 0.03135 * C + 0.0351 * S + 0.646 * OM + 15.29 * D - 0.192 * topSoil - 4.671 * D * D - 0.000781 * C * C - \
-                0.00687 * OM * OM + 0.0449 * (1 / OM) + 0.0663 * np.log(S) + 0.1482 * np.log(OM) - 0.04546 * D * S - 0.4852 * D * OM + 0.00673 * topSoil * C
+                  0.00687 * OM * OM + 0.0449 * (1 / OM) + 0.0663 * np.log(S) + 0.1482 * np.log(
+            OM) - 0.04546 * D * S - 0.4852 * D * OM + 0.00673 * topSoil * C
         return t_alpha
 
     def calculate_transformed_n(self, C, D, S, OM, topSoil):
-        if(topSoil):
+        if (topSoil):
             TS = 1.
         else:
             TS = 0.
-        t_n = -25.23 - 0.02195 * C + 0.0074 * S - 0.1940 * OM + 45.5 * D - 7.24 * D * D + 0.0003658 * C * C + 0.002885 * OM * OM - 12.81 * (1 / D) - \
-                0.1524 * (1 / S) - 0.01958 * (1 / OM) - 0.2876 * np.log(S) - 0.0709 * np.log(OM) - 44.6 * np.log(D) - 0.02264 * D * C + 0.0896 * D * OM + 0.00718 * topSoil * C
+        t_n = -25.23 - 0.02195 * C + 0.0074 * S - 0.1940 * OM + 45.5 * D - 7.24 * D * D + 0.0003658 * C * C + 0.002885 * OM * OM - 12.81 * (
+                    1 / D) - \
+              0.1524 * (1 / S) - 0.01958 * (1 / OM) - 0.2876 * np.log(S) - 0.0709 * np.log(OM) - 44.6 * np.log(
+            D) - 0.02264 * D * C + 0.0896 * D * OM + 0.00718 * topSoil * C
         return t_n
 
     def calculate_transformed_lambda(self, C, D, S, OM, topSoil):
-        if(topSoil):
+        if (topSoil):
             TS = 1.
         else:
             TS = 0.
-        t_lambda = 0.0202 + 0.0006193 * C * C - 0.001136 * OM * OM - 0.2316 * np.log(OM) - 0.03544 * D * C + 0.00283 * D * S + 0.0488 * D * OM;
+        t_lambda = 0.0202 + 0.0006193 * C * C - 0.001136 * OM * OM - 0.2316 * np.log(
+            OM) - 0.03544 * D * C + 0.00283 * D * S + 0.0488 * D * OM;
         return t_lambda
 
     def calculate_transformed_ksat(self, C, D, S, OM, topSoil):
-        if(topSoil):
+        if (topSoil):
             TS = 1.
         else:
             TS = 0.
         t_k_sat = 7.755 + 0.0352 * S + 0.93 * topSoil - 0.967 * D * D - 0.000484 * C * C - 0.000322 * S * S + \
-                0.001 * (1 / S) - 0.0748 * (1 / OM) - 0.643 * np.log(S) - 0.01398 * D * C - 0.1673 * D * OM + \
-                0.02986 * topSoil * C - 0.03305 * topSoil * S
+                  0.001 * (1 / S) - 0.0748 * (1 / OM) - 0.643 * np.log(S) - 0.01398 * D * C - 0.1673 * D * OM + \
+                  0.02986 * topSoil * C - 0.03305 * topSoil * S
         return t_k_sat
 
 
@@ -314,6 +320,7 @@ def make_string_table(XY_table):
     s = s.replace("  ", " ")
     return s
 
+
 def generate_soil_file(longitude, latitude):
     """
     Generate a YAML soil file for given longitude and latitude.
@@ -338,7 +345,9 @@ def generate_soil_file(longitude, latitude):
 
     print(f"YAML soil file has been created at {path_file}.")
 
+
 request_url = "https://rest.isric.org/soilgrids/v2.0/properties/query"
+
 
 def request_soilgrids(lat, lon) -> dict:
     p1 = {"lat": lat, "lon": lon}
@@ -348,8 +357,8 @@ def request_soilgrids(lat, lon) -> dict:
 
     return result
 
-def get_depth_soilgrids() -> list:
 
+def get_depth_soilgrids() -> list:
     zmins, zmaxs = default_zs()
 
     depth_name_template = '{zmin}-{zmax}cm'
@@ -362,8 +371,8 @@ def get_depth_soilgrids() -> list:
 
     return depths
 
-def get_df_soilgrids(lat: float, lon: float) -> pd.DataFrame:
 
+def get_df_soilgrids(lat: float, lon: float) -> pd.DataFrame:
     print(f"getting soilgrids for longitude: {lon} and latitude: {lat}")
     resultd = request_soilgrids(lat, lon)
 
@@ -397,7 +406,8 @@ def get_df_soilgrids(lat: float, lon: float) -> pd.DataFrame:
     df_soilgrids = pd.DataFrame.from_dict(soild)
     return df_soilgrids
 
-def check_value_empty(data, first = True):
+
+def check_value_empty(data, first=True):
     if isinstance(data, dict):
         for key, value in data.items():
             if key == "mean" and value is None:
@@ -417,7 +427,6 @@ def main():
         print("Usage: python generate_soil_file.py <longitude> <latitude>")
         print("Example: python generate_soil_file.py 12.4924 41.8902")
         sys.exit(1)
-
 
     parser = argparse.ArgumentParser(description="Generate a YAML soil file for a given longitude and latitude.")
     parser.add_argument("longitude", type=float, help="Longitude for the soil data.")
