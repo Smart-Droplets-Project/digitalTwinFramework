acece651f7da4d5fca0d718bc2f6c8a2db3f4e2e
Michiel Kallenberg <michielkallenberg@gmail.com>
2025-11-21 16:52:54 +0100

    simple demo

---
+++ digitaltwin/run-demo.py
@@ -0,0 +1,66 @@
+import os
+import datetime
+import uvicorn
+import json
+from apscheduler.schedulers.background import BackgroundScheduler
+from fastapi import FastAPI, Request
+from ngsildclient import SubscriptionBuilder
+
+from sd_data_adapter.client import DAClient
+from sd_data_adapter.api import search
+from sd_data_adapter.models import AgriFood
+from digitaltwin.utils.simulator import run_cropmodel, run_ascabmodel
+from digitaltwin.utils.data_adapter import fill_database, fill_database_ascab
+from digitaltwin.utils.database import clear_database
+
+
+app = FastAPI()
+
+# Host and Port are injected as environment variables
+ORION_HOST = os.environ["ORION_HOST"]
+ORION_PORT = os.environ["ORION_PORT"]
+
+client = DAClient.get_instance(host=ORION_HOST, port=ORION_PORT)
+
+
+@app.post("/notification")
+async def receive_notification(request: Request, debug=False):
+    if debug:
+        headers = request.headers
+        print(f"Received headers: {headers}")
+    try:
+        run_cropmodel(debug=False)
+        return {"status": "received"}
+    except Exception as e:
+        print(f"Error processing request: {e}")
+        return {"status": "error", "message": str(e)}
+
+
+def subscribe_to_ocb(host):
+    MY_SERVER_URL = f"http://{host}:8000/notification"
+
+    subscr = (
+        SubscriptionBuilder(MY_SERVER_URL)
+        .context(
+            "https://raw.githubusercontent.com/smart-data-models/dataModel.AgriFood/master/context.jsonld"
+        )
+        .description("Notification for new AgriParcelOperations.")
+        .select_type("AgriParcelOperation")
+        .build()
+    )
+
+    try:
+        client.subscriptions.create(subscr)
+        print("Subscription created successfully.")
+    except Exception as e:
+        print(f"Failed to create subscription: {e}a")
+
+
+if __name__ == "__main__":
+    my_host = os.environ["DIGITAL_TWIN_HOST"]
+    subscribe_to_ocb(my_host)
+
+    clear_database()
+    fill_database()
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
